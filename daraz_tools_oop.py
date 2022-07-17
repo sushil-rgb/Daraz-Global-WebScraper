8962bb15f82c5e0463da5d922e0371e117b0e43a
sus0001 <sushil.bhandari002@gmail.com>
2022-07-18 02:26:32 +0545

    bugs fixed

--- daraz_tools_oop.py
+++ daraz_tools_oop.py
@@ -44,21 +44,20 @@ class DarazScraper:
         with sync_playwright() as p:
             self.browser = p.chromium.launch(headless=True, slow_mo=1*1000)
             self.page = self.browser.new_page(user_agent=self.headers)
-            self.page.goto(self.base_url)           
-
+            self.page.goto(self.base_url) 
             
             try:
                 self.page.wait_for_url(self.base_url)
                 self.query_name_selector = "//h1[@class='title--Xj2oo']"
-
                 self.page.wait_for_selector(self.query_name_selector, timeout=10000)
                 self.categ_name = self.page.query_selector(self.query_name_selector).inner_text().strip()
-                self.browser.close()
 
+                self.browser.close()
                 return self.categ_name
             except Exception as e:
                 print(f"Playwright Script error. | {self.page}")
                 traceback.print_exc()
+
                 self.browser.close()  
                
     
@@ -67,16 +66,14 @@ class DarazScraper:
             self.browser = p.chromium.launch(headless=True, slow_mo=1*1000)
             self.page = self.browser.new_page(user_agent=self.headers)
             self.page.goto(self.base_url)
-
             self.last_page_number = self.page.query_selector_all("//li[@tabindex='0']")
             self.last_one = int(self.last_page_number[len(self.last_page_number)-2].get_attribute('title'))
-
-            return self.last_one
             
+            self.browser.close()
+            return self.last_one            
 
 
-    def scrapeLinksNamesPrices(self):
-        # Setting up the Playwright driver:
+    def scrapeLinksNamesPrices(self):        
         with sync_playwright() as p:
             self.browser = p.chromium.launch(headless=True, slow_mo=1*5000)
             self.page = self.browser.new_page(user_agent=self.headers)
@@ -86,28 +83,27 @@ class DarazScraper:
             # The split could be done cleaner by using regex, this one is ugly:
             self.letsplit = self.base_url.split("?")[-1].split("&")[0].split("=")[-1]
             print(f"Scraping page number | {self.letsplit}\n-----------------------------")
-
                 
             try:                
                 self.link_selector = "//div[@class='title--wFj93']/a"
-                self.page.wait_for_selector(self.link_selector, timeout=10000)
+                self.page.wait_for_selector(self.link_selector, timeout=5000)
                 self.hyper_links = [f"https:{link.get_attribute('href')}" for link in self.page.query_selector_all(self.link_selector)]
             except PlaywrightTimeoutError:
                 print("Timeout error. Reloading the page.")
                 self.page.reload()
                 self.link_selector = "//div[@class='title--wFj93']/a"
-                self.page.wait_for_selector(self.link_selector, timeout=10000)
+                self.page.wait_for_selector(self.link_selector, timeout=5000)
                 self.hyper_links = [f"https:{link.get_attribute('href')}" for link in self.page.query_selector_all(self.link_selector)]  
               
             self.prod_selector = '//div[@class="title--wFj93"]'
-            self.page.wait_for_selector(self.prod_selector, timeout=10000)
+            self.page.wait_for_selector(self.prod_selector, timeout=5000)
             
             self.prod_query_selector = self.page.query_selector_all(self.prod_selector)
             
             self.prod_names = [name.inner_text() for name in self.prod_query_selector] 
             
             self.price_selector = "//span[@class='currency--GVKjl']"
-            self.page.wait_for_selector(self.price_selector, timeout=10000)
+            self.page.wait_for_selector(self.price_selector, timeout=5000)
 
             self.prod_prices = [price.inner_text() for price in self.page.query_selector_all(self.price_selector)]
            
@@ -133,17 +129,17 @@ class DarazIndivLinkScraper:
                 try:
                     self.name_selector = "//span[@class='pdp-mod-product-badge-title']"
                     self.page.wait_for_selector(self.name_selector, timeout=1*10000)
-
                     self.name = self.page.query_selector(self.name_selector).inner_text().strip()
-                    self.browser.close()
 
+                    self.browser.close()
                     return self.name   
                 except TypeError:
                     self.name = "N/A"
                     return self.name             
             except Exception as e:
                 print(f"Error in plawright script {self.page}")   
-                traceback.print_exc()             
+                traceback.print_exc()     
+
                 self.browser.close()
     
 
@@ -158,10 +154,9 @@ class DarazIndivLinkScraper:
                 try:
                     self.price_selector = "//span[@class=' pdp-price pdp-price_type_normal pdp-price_color_orange pdp-price_size_xl']"
                     self.page.wait_for_selector(self.price_selector, timeout=1*10000)
-
                     self.price = self.page.query_selector(self.price_selector).inner_text().strip()
-                    self.browser.close()
 
+                    self.browser.close()
                     return self.price
                 except TypeError:
                     self.price = "N/A"
@@ -182,10 +177,9 @@ class DarazIndivLinkScraper:
                 try:
                     self.og_selector = "//span[@class=' pdp-price pdp-price_type_deleted pdp-price_color_lightgray pdp-price_size_xs']"
                     self.page.wait_for_selector(self.og_selector)
-
                     self.ogPrice = self.page.query_selector(self.og_selector).inner_text().strip()
-                    self.browser.close()
 
+                    self.browser.close()
                     return self.ogPrice
                 except TypeError:
                     self.ogPrice = "N/A"
